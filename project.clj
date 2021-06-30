278c388fb1553c859efdd5ecfdc3f67cca69c612
vemv <vemv@users.noreply.github.com>
2021-06-30 10:02:54 +0200

    try thing

--- project.clj
+++ project.clj
@@ -9,6 +9,7 @@
                  ^:inline-dep [http-kit "2.5.1"]
                  ^:inline-dep [http-kit "2.5.1"]
                  [nrepl "0.8.3"]
+                 ^:inline-dep [cider/piggieback "0.5.2"]
                  ^:inline-dep [org.clojure/data.json "2.3.1"]
                  ^:inline-dep [org.clojure/tools.analyzer.jvm "1.1.0"]
                  ^:inline-dep [org.clojure/tools.namespace "1.1.0" :exclusions [org.clojure/tools.reader]]
@@ -45,10 +46,8 @@
              :test {:dependencies [[print-foo "1.0.2"]]
                     :src-paths ["test/resources"]}
              :dev {:global-vars {*warn-on-reflection* true}
-                   :dependencies [[cider/piggieback "0.5.2"]
-                                  [commons-io/commons-io "2.8.0"]
+                   :dependencies [[commons-io/commons-io "2.8.0"]
                                   [org.clojure/clojurescript "1.9.946"]]
-                   :repl-options {:nrepl-middleware [cider.piggieback/wrap-cljs-repl]}
                    :java-source-paths ["test/java"]
                    :resource-paths ["test/resources"
                                     "test/resources/testproject/src"]
